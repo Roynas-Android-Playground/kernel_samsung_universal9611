e0149bacfc79a81331c1a5bfa90508f8ecc17316
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-11-06 10:36:32 +0530

    input: Silence input_info logs when CONFIG_SEC_DEBUG_TSP_LOG is disabled
    Change-Id: I542b45ef890707b2c1e4b015324d9ec35c4366f6
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- include/uapi/linux/input.h
+++ include/uapi/linux/input.h
@@ -110,7 +110,7 @@
 	({										\
 		static char input_log_buf[INPUT_LOG_BUF_SIZE];				\
 		snprintf(input_log_buf, sizeof(input_log_buf), "%s %s", SECLOG, fmt);	\
-		dev_info(dev, input_log_buf, ## __VA_ARGS__);				\
+		dev_dbg(dev, input_log_buf, ## __VA_ARGS__);				\
 	 })
 #define input_err(mode, dev, fmt, ...)						\
 	({										\
