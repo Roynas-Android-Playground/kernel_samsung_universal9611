c505994e9bc83e78732db4563ccf12e0aa7c6361
Tim Zimmermann <tim@linux4.de>
2022-12-10 20:21:49 +0100

    misc: tzdev: Support fallback tzar
    * Older TEE is required for F41
    
    Change-Id: Ifb228b6d024fdf6ff39f272b29ac2a53420861e3
    Signed-off-by: roynatech2544 <whiteshell2544@naver.com>

--- drivers/misc/tzdev/tz_deploy_tzar.c
+++ drivers/misc/tzdev/tz_deploy_tzar.c
@@ -38,6 +38,15 @@ __asm__ (
 );
 extern char tzdev_tzar_begin[], tzdev_tzar_end[];
 
+__asm__ (
+	".section .init.data,\"aw\"\n"
+	"tzdev_old_tzar_begin:\n"
+	".incbin \"" KBUILD_SRC "/drivers/misc/tzdev/startup_old.tzar\"\n"
+	"tzdev_old_tzar_end:\n"
+	".previous\n"
+);
+extern char tzdev_old_tzar_begin[], tzdev_old_tzar_end[];
+
 enum iw_startup_loader_cmd_type {
 	CMD_STARTUP_LOADER_INVALID_ID,
 	CMD_STARTUP_LOADER_UPLOAD_CONTAINER,
@@ -58,27 +67,35 @@ struct cmd_startup_loader_reply_upload_container {
 static __ref int tz_deploy_handler(void *arg)
 {
 	int ret;
-	size_t size;
+	size_t size, old_size;
 	struct sock_desc *sd;
 	struct cmd_startup_loader_upload_container command;
 	struct cmd_startup_loader_reply_upload_container reply;
 	void *tzar;
 	unsigned int attempt;
+	bool use_new_tzar = true;
 	(void)arg;
 
 	size = tzdev_tzar_end - tzdev_tzar_begin;
-	tzdev_print(0, "[debug] tzar size = %zu\n", size);
+	old_size = tzdev_old_tzar_end - tzdev_old_tzar_begin;
+	tzdev_print(0, "[debug] tzar size = %zu, old_tzar size = %zu\n", size, old_size);
 
-	tzar = vmalloc(size);
+reload:
+	tzar = use_new_tzar ? vmalloc(size) : vmalloc(old_size);
 	if (!tzar) {
 		tzdev_print(0, "vmalloc fail\n");
 		return -ENOMEM;
 	}
 
-	memcpy(tzar, tzdev_tzar_begin, size);
+	if (use_new_tzar) {
+		memcpy(tzar, tzdev_tzar_begin, size);
+	} else {
+		memcpy(tzar, tzdev_old_tzar_begin, old_size);
+	}
 
 	size = DIV_ROUND_UP(size, PAGE_SIZE) * PAGE_SIZE;
-	ret = tzdev_mem_register(tzar, size, 0, NULL, NULL);
+	old_size = DIV_ROUND_UP(old_size, PAGE_SIZE) * PAGE_SIZE;
+	ret = tzdev_mem_register(tzar, use_new_tzar ? size : old_size, 0, NULL, NULL);
 	if (ret < 0) {
 		tzdev_print(0, "tzdev_mem_register fail, ret=%d\n", ret);
 		goto out_tzar;
@@ -86,7 +103,7 @@ static __ref int tz_deploy_handler(void *arg)
 
 	command.base.cmd = CMD_STARTUP_LOADER_UPLOAD_CONTAINER;
 	command.buf_desc.id = ret;
-	command.buf_desc.size = (unsigned int)size;
+	command.buf_desc.size = use_new_tzar ? (unsigned int)size : (unsigned int)old_size;
 
 	sd = tz_iwsock_socket(1);
 	if (IS_ERR(sd)) {
@@ -123,8 +140,14 @@ static __ref int tz_deploy_handler(void *arg)
 		goto out_sock;
 	}
 
-	if (reply.base.result)
+	if (reply.base.result) {
+		if (use_new_tzar) {
+			use_new_tzar = false;
+			tzdev_print(0, "New startup.tzar failed to load, try older\n");
+			goto reload;
+		}
 		panic("tzdev: iwsock error. result=%d\n", reply.base.result);
+	}
 
 	if (reply.base.base.cmd != CMD_STARTUP_LOADER_REPLY_UPLOAD_CONTAINER)
 		panic("tzdev: iwsock wrong cmd. cmd=%u\n", reply.base.base.cmd);
