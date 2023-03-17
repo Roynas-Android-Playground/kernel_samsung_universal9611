94564ab6b3525a8cd6ed132022626a91cf9305bc
roynatech2544 <whiteshell2544@naver.com>
2023-03-17 02:07:20 +0000

    melfas/mss100: Enable DeX support only for OneUI builds
    - On AOSP it is useless

--- drivers/input/touchscreen/melfas/mss100/melfas_mss100.c
+++ drivers/input/touchscreen/melfas/mss100/melfas_mss100.c
@@ -1411,7 +1411,7 @@ static void mms_read_info_work(struct work_struct *work)
 	mms_run_rawdata(info, true);
 	info->info_work_done = true;
 }
-
+#ifdef MMS_SUPPORT_DEX
 static void mms_set_input_prop_pad(struct mms_ts_info *info, struct input_dev *dev)
 {
 	static char ist_phys[64] = { 0 };
@@ -1441,7 +1441,7 @@ static void mms_set_input_prop_pad(struct mms_ts_info *info, struct input_dev *d
 
 	input_mt_init_slots(dev, 10, INPUT_MT_POINTER);
 }
-
+#endif
 static void mms_set_input_prop_proximity(struct mms_ts_info *info, struct input_dev *dev)
 {
 	static char mms_phys[64] = { 0 };
@@ -1662,7 +1662,7 @@ static int mms_probe(struct i2c_client *client, const struct i2c_device_id *id)
 
 	mms_init_config(info);
 	mms_config_input(info);
-
+#ifdef MMS_SUPPORT_DEX
 	info->input_dev_pad = input_allocate_device();
 	if (!info->input_dev_pad) {
 		input_err(true, &client->dev, "%s: allocate device err!\n", __func__);
@@ -1676,7 +1676,7 @@ static int mms_probe(struct i2c_client *client, const struct i2c_device_id *id)
 		input_err(true, &client->dev, "%s: Unable to register %s input device\n", __func__, info->input_dev_pad->name);
 		goto err_input_pad_register_device;
 	}
-
+#endif
 #ifdef USE_TSP_TA_CALLBACKS
 	info->register_cb = mms_register_callback;
 	info->callbacks.inform_charger = mms_charger_status_cb;
@@ -1797,11 +1797,13 @@ err_test_dev_create:
 	mms_disable(info);
 	free_irq(info->irq, info);
 err_request_irq:
+#ifdef MMS_SUPPORT_DEX
 	input_unregister_device(info->input_dev_pad);
 	info->input_dev_pad = NULL;
 err_input_pad_register_device:
 	if (info->input_dev_pad)
 		input_free_device(info->input_dev_pad);
+#endif
 err_fw_update:
 	mms_power_control(info, 0);
 	if (info->dtdata->support_ear_detect) {
@@ -1872,10 +1874,10 @@ static int mms_remove(struct i2c_client *client)
 		input_mt_destroy_slots(info->input_dev_proximity);
 		input_unregister_device(info->input_dev_proximity);
 	}
-	
+#ifdef MMS_SUPPORT_DEX
 	input_mt_destroy_slots(info->input_dev_pad);
 	input_unregister_device(info->input_dev_pad);
-
+#endif
 	input_mt_destroy_slots(info->input_dev);
 	input_unregister_device(info->input_dev);
 
